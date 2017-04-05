39483202db40ea935dfc63d1bd1ccff9465a56f5
Sonam <sonam@headerlabs.com>
2017-04-05 13:45:52 +0530

    updated html and css files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,48 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent,
+    RegisterComponent,
+    
+    LogInComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+
+      
+
+      {
+        path: 'log-in',
+        component: LogInComponent,
+      },
+      {
+        path: 'about-us',
+        component: AboutUsComponent,
+      },
+          ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
